9bb416e389ffa56db53942e174df060da1284270
andela-moseni <mercy.oseni@andela.com>
2017-04-22 13:50:46 +0100

    feature(Roles): modify method names

--- server/routes/rolesRoute.js
+++ server/routes/rolesRoute.js
@@ -3,14 +3,14 @@ import RolesController from '../controllers/RolesController';
 const rolesRoute = (router) => {
   // Create a new role or get all roles
   router.route('/roles')
-   .post(RolesController.create)
-   .get(RolesController.list);
+   .post(RolesController.createRole)
+   .get(RolesController.listRoles);
 
   // Get a particular role, update a role and delete a role
   router.route('/roles/:id')
-  .get(RolesController.retrieve)
-  .put(RolesController.update)
-  .delete(RolesController.delete);
+  .get(RolesController.retrieveRole)
+  .put(RolesController.updateRole)
+  .delete(RolesController.deleteRole);
 };
 
 export default rolesRoute;
