ddb24a88a543e9a03382480e07c861c3774e608f
andela-moseni <mercy.oseni@andela.com>
2017-06-09 07:53:06 +0100

    modify edit-role modal

--- client/components/roles/RoleListRow.jsx
+++ client/components/roles/RoleListRow.jsx
@@ -36,27 +36,31 @@ class RoleListRow extends React.Component {
     const date = new Date(role.createdAt);
     const editBtn = (
       <button
-        className="btn-floating btn-large waves-effect waves-light">
+        className="btn-floating btn-large waves-effect waves-light"
+      >
         <i className="material-icons">edit</i>
       </button>
     );
     const deleteBtn = (
       <button
-        className="btn-floating btn-large waves-effect waves-light cyan">
+        className="btn-floating btn-large waves-effect waves-light cyan"
+      >
         <i className="material-icons red">delete</i>
       </button>
     );
 
     return (
       <tr>
-      <td> {serial} </td>
-      <td>{role.title}</td>
-      <td> {date.toDateString()} </td>
-      <td> {newDate.toDateString()} </td>
-      <td>
-        <div className="center">
-            <Modal header="Update Role"
-            trigger={editBtn} id={`modal_${role.id}`}>
+        <td> {serial} </td>
+        <td>{role.title}</td>
+        <td> {date.toDateString()} </td>
+        <td> {newDate.toDateString()} </td>
+        <td>
+          <div className="center">
+            <Modal
+              header="Update Role"
+              trigger={editBtn}
+            >
               <form className="col s12" onSubmit={this.onSubmit}>
                 <div className="row">
                   <TextFieldGroup
@@ -69,22 +73,24 @@ class RoleListRow extends React.Component {
                     placeholder="alphabets only"
                   />
 
-                  <button className="btn waves-effect waves-light submitBtn"
-                    type="submit" name="action">Update
-                    <i className="material-icons right">send</i>
+                  <button
+                    className="btn waves-effect waves-light submitBtn"
+                    type="submit" name="action"
+                  >Update
+                      <i className="material-icons right">send</i>
                   </button>
                 </div>
               </form>
             </Modal>
-        </div>
-      </td>
-      <td>
-        <Prompt
-        trigger={deleteBtn}
-        onClickFunction={this.deleteRole}
-      />
-      </td>
-    </tr>
+          </div>
+        </td>
+        <td>
+          <Prompt
+            trigger={deleteBtn}
+            onClickFunction={this.deleteRole}
+          />
+        </td>
+      </tr>
     );
   }
 }
